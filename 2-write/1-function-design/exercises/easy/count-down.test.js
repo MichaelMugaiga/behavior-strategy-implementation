58a53c33bbd0e122bead5978473a4d1d0baa768e
Mugaiga <mmugaiga@gmail.com>
2022-11-08 21:13:19 +0100

    count down exercise done

--- 2-write/1-function-design/exercises/easy/count-down.test.js
+++ 2-write/1-function-design/exercises/easy/count-down.test.js
@@ -10,8 +10,25 @@
  */
 
 // -------- your solutions --------
+const countDown = (start = 0) => {
+let result  = [];
+  if(start < 0) {
+    throw new RangeError('number is not positive');
+  }
+  if(!Number.isInteger(start)) {
+    throw new TypeError('number is not an integer');
+  }
+  if (typeof start === 'string') {
+    throw Error("start should be a number");
+  } 
+  for (let step = start; step >= 0; step--) {
+    result.push(step);
+  }
+  return result;
+}
 
-for (const solution of [secretSolution]) {
+// eslint-disable-next-line no-restricted-syntax
+for (const solution of [secretSolution ,countDown]) {
   // the main test suite for the function
   describe(solution.name + ': counts down to 0', () => {
     it('default parameter is 0 -> [0]', () => {
@@ -24,6 +41,21 @@ for (const solution of [secretSolution]) {
       expect(solution(1)).toEqual([1, 0]);
     });
     // write at least 5 more tests ...
+    it('2 -> [2,1,0]', () => {
+      expect(solution(2)).toEqual([2, 1, 0]);
+    });
+    it('3 -> [3, 2, 1, 0]', () => {
+      expect(solution(3)).toEqual([3, 2, 1, 0]);
+    });
+    it('should throw RangeError when start is not a positive number',() => {
+      expect(()=> solution(-1)).toThrow(RangeError);
+    });
+    it('should throw TypeError when start is not a number',() => {
+      expect(()=> solution("2")).toThrow(TypeError);
+    });
+    it('should throw Error when start is not an integer',() => {
+      expect(()=> solution(6.5)).toThrow(Error);
+    });
   });
 }
 
